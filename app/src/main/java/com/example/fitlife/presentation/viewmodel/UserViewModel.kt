159e2085c5e6840fb2d7539a8220120d29f8e196
Mafe sanchez <maria.sanchez02@uptc.edu.co>
2024-11-29 23:29:26 -0500

    Add user view model of add  and load

--- app/src/main/java/com/example/fitlife/presentation/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/fitlife/presentation/viewmodel/UserViewModel.kt
@@ -2,18 +2,22 @@ package com.example.fitlife.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.fitlife.data.remote.firebase.services.UserService
 import com.example.fitlife.data.repository.AuthRepositoryImpl
 import com.example.fitlife.presentation.ui.screens.states.UserUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+
 @HiltViewModel
 class UserViewModel @Inject constructor(
-    private val repository: AuthRepositoryImpl
+    private val repository: AuthRepositoryImpl,
+    private val userService: UserService
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(UserUiState())
@@ -23,5 +27,30 @@ class UserViewModel @Inject constructor(
         viewModelScope.launch {
         }
     }
+
+    fun addCompletedExercise(exerciseName: String) {
+        val uid = _uiState.value.user?.uid ?: return
+        viewModelScope.launch {
+            val success = userService.addCompletedExercise(uid, exerciseName)
+            if (success) {
+                _uiState.update { state ->
+                    state.copy(
+                        completedExercises = state.completedExercises + exerciseName
+                    )
+                }
+            }
+        }
+    }
+
+    fun loadCompletedExercises() {
+        val uid = _uiState.value.user?.uid ?: return
+        viewModelScope.launch {
+            val exercises = userService.getCompletedExercises(uid)
+            _uiState.update { state ->
+                state.copy(completedExercises = exercises)
+            }
+        }
+    }
+
 }
 
